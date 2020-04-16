f446fdf218e6550d78bc8aa334f5aecc09bec7cf
work <it@af-bogorodsk.ru>
2020-04-16 13:24:21 +0300

    server

---
+++ utils/sequelizeConnect.js
@@ -0,0 +1,23 @@
+const Sequelize = require('sequelize')
+const paramConnect = require('../config/keys')
+
+const sequelize = new Sequelize(
+                        paramConnect.dbName,
+                        paramConnect.dbUser,
+                        paramConnect.dbPassword,
+                        {
+                            host: paramConnect.dbHost,
+                            dialect: paramConnect.dbDialect,
+                            pool: {
+                                max: 5,
+                                min: 0,
+                                acquire: 30000,
+                                idle: 10000
+                            },
+                            define: {
+                                timestamps: false
+                            }
+                        }
+                    )
+
+module.exports = sequelize
